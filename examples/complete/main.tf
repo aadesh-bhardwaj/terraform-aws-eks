316de141647b32d991f46d2bf13c20d62615843a
Aadesh Bhardwaj <122770344+aadesh-bhardwaj@users.noreply.github.com>
2025-05-16 03:29:28 +0530

    Update main.tf

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -51,26 +51,26 @@ data "aws_caller_identity" "current" {}
 module "kms" {
   source                  = "terraform-aws-modules/kms/aws"
   version                 = "3.1.0"
-  deletion_window_in_days = local.kms_deletion_window_in_days
-  description             = "Symetric Key to Enable Encryption at rest using KMS services."
-  enable_key_rotation     = local.kms_key_rotation_enabled
-  is_enabled              = local.is_enabled
+  deletion_window_in_days = var.kms_deletion_window_in_days
+  description             = "Symmetric Key to Enable Encryption at rest using KMS services."
+  enable_key_rotation     = var.kms_key_rotation_enabled
+  is_enabled              = var.is_enabled
   key_usage               = "ENCRYPT_DECRYPT"
-  multi_region            = local.multi_region
+  multi_region            = var.multi_region
 
-  # Policy
-  enable_default_policy                  = true
-  key_owners                             = [local.current_identity]
-  key_administrators                     = local.kms_user == null ? ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling", "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/eks.amazonaws.com/AWSServiceRoleForAmazonEKS", local.current_identity] : local.kms_user
-  key_users                              = local.kms_user == null ? ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling", "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/eks.amazonaws.com/AWSServiceRoleForAmazonEKS", local.current_identity] : local.kms_user
-  key_service_users                      = local.kms_user == null ? ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling", "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/eks.amazonaws.com/AWSServiceRoleForAmazonEKS", local.current_identity] : local.kms_user
-  key_symmetric_encryption_users         = [local.current_identity]
-  key_hmac_users                         = [local.current_identity]
-  key_asymmetric_public_encryption_users = [local.current_identity]
-  key_asymmetric_sign_verify_users       = [local.current_identity]
+  enable_default_policy  = true
+  key_owners             = [data.aws_caller_identity.current.arn]
+  key_administrators     = var.kms_user == null ? ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling", "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/eks.amazonaws.com/AWSServiceRoleForAmazonEKS", data.aws_caller_identity.current.arn] : var.kms_user
+  key_users              = var.kms_user == null ? ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling", "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/eks.amazonaws.com/AWSServiceRoleForAmazonEKS", data.aws_caller_identity.current.arn] : var.kms_user
+  key_service_users      = var.kms_user == null ? ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling", "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/eks.amazonaws.com/AWSServiceRoleForAmazonEKS", data.aws_caller_identity.current.arn] : var.kms_user
+  key_symmetric_encryption_users = [data.aws_caller_identity.current.arn]
+  key_hmac_users                = [data.aws_caller_identity.current.arn]
+  key_asymmetric_public_encryption_users = [data.aws_caller_identity.current.arn]
+  key_asymmetric_sign_verify_users       = [data.aws_caller_identity.current.arn]
+
   key_statements = [
     {
-      sid    = "AllowCloudWatchLogsEncryption",
+      sid    = "AllowCloudWatchLogsEncryption"
       effect = "Allow"
       actions = [
         "kms:Encrypt*",
@@ -80,96 +80,81 @@ module "kms" {
         "kms:Describe*"
       ]
       resources = ["*"]
-
       principals = [
         {
           type        = "Service"
-          identifiers = ["logs.${local.region}.amazonaws.com"]
+          identifiers = ["logs.${var.region}.amazonaws.com"]
         }
       ]
     }
   ]
-  # Aliases
-  aliases                 = ["${local.name}-KMS"]
+
+  aliases                 = ["${var.name}-KMS"]
   aliases_use_name_prefix = true
 }
 
 module "key_pair_vpn" {
   source             = "squareops/keypair/aws"
   version            = "1.0.2"
-  count              = local.vpn_server_enabled ? 1 : 0
-  key_name           = format("%s-%s-vpn", local.environment, local.name)
-  environment        = local.environment
-  ssm_parameter_path = format("%s-%s-vpn", local.environment, local.name)
+  count              = var.vpn_server_enabled ? 1 : 0
+  key_name           = format("%s-%s-vpn", var.environment, var.name)
+  environment        = var.environment
+  ssm_parameter_path = format("%s-%s-vpn", var.environment, var.name)
 }
 
 module "key_pair_eks" {
   source             = "squareops/keypair/aws"
   version            = "1.0.2"
-  key_name           = format("%s-%s-eks", local.environment, local.name)
-  environment        = local.environment
-  ssm_parameter_path = format("%s-%s-eks", local.environment, local.name)
+  key_name           = format("%s-%s-eks", var.environment, var.name)
+  environment        = var.environment
+  ssm_parameter_path = format("%s-%s-eks", var.environment, var.name)
 }
 
 module "vpc" {
-  source                                          = "squareops/vpc/aws"
-  version                                         = "3.4.1"
-  name                                            = local.name
-  region                                          = local.region
-  vpc_cidr                                        = local.vpc_cidr
-  environment                                     = local.environment
-  vpn_key_pair_name                               = local.vpn_server_enabled ? module.key_pair_vpn[0].key_pair_name : null
-  availability_zones                              = local.vpc_availability_zones
-  intra_subnet_enabled                            = local.vpc_intra_subnet_enabled
-  public_subnet_enabled                           = local.vpc_public_subnet_enabled
-  auto_assign_public_ip                           = local.auto_assign_public_ip
-  private_subnet_enabled                          = local.vpc_private_subnet_enabled
-  one_nat_gateway_per_az                          = local.vpc_one_nat_gateway_per_az
-  database_subnet_enabled                         = local.vpc_database_subnet_enabled
-  vpn_server_enabled                              = local.vpn_server_enabled
-  vpn_server_instance_type                        = "t3a.small"
-  vpc_s3_endpoint_enabled                         = local.vpc_s3_endpoint_enabled
-  vpc_ecr_endpoint_enabled                        = local.vpc_ecr_endpoint_enabled
-  flow_log_enabled                                = local.vpc_flow_log_enabled
-  flow_log_max_aggregation_interval               = 60 # In seconds
+  source                      = "squareops/vpc/aws"
+  version                     = "3.4.1"
+  name                        = var.name
+  region                      = var.region
+  vpc_cidr                    = var.vpc_cidr
+  environment                 = var.environment
+  vpn_key_pair_name           = var.vpn_server_enabled ? module.key_pair_vpn[0].key_pair_name : null
+  availability_zones          = var.vpc_availability_zones
+  intra_subnet_enabled        = var.vpc_intra_subnet_enabled
+  public_subnet_enabled       = var.vpc_public_subnet_enabled
+  auto_assign_public_ip       = var.auto_assign_public_ip
+  private_subnet_enabled      = var.vpc_private_subnet_enabled
+  one_nat_gateway_per_az      = var.vpc_one_nat_gateway_per_az
+  database_subnet_enabled     = var.vpc_database_subnet_enabled
+  vpn_server_enabled          = var.vpn_server_enabled
+  vpn_server_instance_type    = var.vpn_server_instance_type
+  vpc_s3_endpoint_enabled     = var.vpc_s3_endpoint_enabled
+  vpc_ecr_endpoint_enabled    = var.vpc_ecr_endpoint_enabled
+  flow_log_enabled            = var.vpc_flow_log_enabled
+  flow_log_max_aggregation_interval               = 60
   flow_log_cloudwatch_log_group_skip_destroy      = false
   flow_log_cloudwatch_log_group_retention_in_days = 90
-  flow_log_cloudwatch_log_group_kms_key_arn       = module.kms.key_arn #Enter your kms key arn
+  flow_log_cloudwatch_log_group_kms_key_arn       = module.kms.key_arn
 }
 
 module "eks" {
-  source               = "squareops/eks/aws"
-  version              = "5.4.4"
-  access_entry_enabled = false
-  access_entries = {
-    "example" = {
-      kubernetes_groups = []
-      principal_arn     = "arn:aws:iam::767398031518:user/aadesh-test"
-      policy_associations = {
-        example = {
-          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-          access_scope = {
-            namespaces = []
-            type       = "cluster"
-          }
-        }
-      }
-    }
-  }
+  source                                = "squareops/eks/aws"
+  version                               = "5.4.4"
+  access_entry_enabled                  = false
+  access_entries                        = {}
   enable_cluster_creator_admin_permissions = true
-  authentication_mode                      = "API_AND_CONFIG_MAP"
-  name                                     = local.name
-  vpc_id                                   = module.vpc.vpc_id
-  environment                              = local.environment
-  kms_key_arn                              = module.kms.key_arn
-  cluster_version                          = local.cluster_version
-  cluster_log_types                        = local.cluster_log_types
-  vpc_private_subnet_ids                   = module.vpc.private_subnets
-  cluster_log_retention_in_days            = local.cluster_log_retention_in_days
-  cluster_endpoint_public_access           = local.cluster_endpoint_public_access
-  cluster_endpoint_public_access_cidrs     = local.cluster_endpoint_public_access_cidrs
-  cluster_endpoint_private_access          = local.cluster_endpoint_private_access
-  nodes_keypair_name                       = module.key_pair_eks.key_pair_name
+  authentication_mode                   = "API_AND_CONFIG_MAP"
+  name                                  = var.name
+  vpc_id                                = module.vpc.vpc_id
+  environment                           = var.environment
+  kms_key_arn                           = module.kms.key_arn
+  cluster_version                       = var.cluster_version
+  cluster_log_types                     = var.cluster_log_types
+  vpc_private_subnet_ids                = module.vpc.private_subnets
+  cluster_log_retention_in_days         = var.cluster_log_retention_in_days
+  cluster_endpoint_public_access        = var.cluster_endpoint_public_access
+  cluster_endpoint_public_access_cidrs  = var.cluster_endpoint_public_access_cidrs
+  cluster_endpoint_private_access       = var.cluster_endpoint_private_access
+  nodes_keypair_name                    = module.key_pair_eks.key_pair_name
   cluster_security_group_additional_rules = {
     ingress_port_mgmt_tcp = {
       description = "mgmt vpc cidr"
@@ -177,12 +162,12 @@ module "eks" {
       from_port   = 443
       to_port     = 443
       type        = "ingress"
-      cidr_blocks = ["10.10.0.0/16"]
+      cidr_blocks = [var.vpc_cidr]
     }
   }
   enable_vpc_cni_addon = true
   vpc_cni_version      = "v1.19.3-eksbuild.1"
-  tags                 = local.additional_aws_tags
+  tags                 = var.additional_aws_tags
 }
 
 module "managed_node_group_addons" {
@@ -194,13 +179,13 @@ module "managed_node_group_addons" {
   managed_ng_max_size         = 5
   managed_ng_desired_size     = 2
   vpc_subnet_ids              = [module.vpc.private_subnets[0]]
-  environment                 = local.environment
+  environment                 = var.environment
   managed_ng_kms_key_arn      = module.kms.key_arn
-  managed_ng_capacity_type    = local.managed_ng_capacity_type
-  managed_ng_ebs_volume_size  = local.ebs_volume_size
+  managed_ng_capacity_type    = var.managed_ng_capacity_type
+  managed_ng_ebs_volume_size  = var.ebs_volume_size
   managed_ng_ebs_volume_type  = "gp3"
   managed_ng_ebs_encrypted    = true
-  managed_ng_instance_types   = ["t3a.large", "t3.large", "t3.medium"] # Pass instance type according to the ami architecture.
+  managed_ng_instance_types   = ["t3a.large", "t3.large", "t3.medium"]
   managed_ng_kms_policy_arn   = module.eks.kms_policy_arn
   associate_public_ip_address = false
   enable_coredns_addon        = true
@@ -214,14 +199,14 @@ module "managed_node_group_addons" {
   eks_nodes_keypair_name        = module.key_pair_eks.key_pair_name
   managed_ng_pod_capacity       = 90
   managed_ng_monitoring_enabled = true
-  launch_template_name          = local.launch_template_name
+  launch_template_name          = var.launch_template_name
   k8s_labels = {
     "Addons-Services" = "true"
   }
-  tags                        = local.additional_aws_tags
-  custom_ami_id               = ""                                # Optional, if not passed terraform will automatically select the latest supported ami id
-  aws_managed_node_group_arch = local.aws_managed_node_group_arch # optional if "custom_ami_id" is passed
-  enable_bottlerocket_ami     = local.enable_bottlerocket_ami     # Set it to false if using Amazon Linux AMIs
+  tags                        = var.additional_aws_tags
+  custom_ami_id               = ""
+  aws_managed_node_group_arch = var.aws_managed_node_group_arch
+  enable_bottlerocket_ami     = var.enable_bottlerocket_ami
   bottlerocket_node_config = {
     bottlerocket_eks_node_admin_container_enabled = false
     bottlerocket_eks_enable_control_container     = true
