3170875052f8830ef844cbd2ae950facf85f2313
Aadesh Bhardwaj <122770344+aadesh-bhardwaj@users.noreply.github.com>
2025-07-08 20:39:30 +0530

    Update provider.tf

--- examples/complete/provider.tf
+++ examples/complete/provider.tf
@@ -3,16 +3,19 @@ provider "aws" {
   default_tags {
     tags = local.additional_aws_tags
   }
+  assume_role {
+    role_arn    = var.role_arn
+    external_id = var.external_id
+    duration    = "30m"
+  }
 }
 
-provider "kubernetes" = {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    # This requires the awscli to be installed locally where Terraform is executed
-    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "= 5.99.1"
+    }
   }
 }
